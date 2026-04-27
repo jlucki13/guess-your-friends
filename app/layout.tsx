4760763588475c238bdac84e10acb136cef53dc3
Lucki <jordan.lucki@accenture.com>
2026-04-27 09:49:42 -0400

    updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Guess Your Friends",
+  title: "Guess Your Friendsssssss",
   description: "The party game where you guess what your friends said",
 };
 
